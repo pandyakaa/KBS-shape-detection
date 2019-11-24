71e8ddb5e4e636af365657bc7a1bf8d60077ec6e
gardahadi <garda4199@gmail.com>
2019-11-25 03:36:29 +0700

    fixed hexagon and pentagon

--- main.py
+++ main.py
@@ -15,16 +15,19 @@ for rule in rules :
     env.build(rule)
 
 
-env.assert_string('(adjacent 3)')
+env.assert_string('(adjacent 6)')
 env.assert_string('(line (integer 1) (integer 5) )')
 env.assert_string('(line (integer 2) (integer 5) )')
 env.assert_string('(line (integer 3) (integer 5) )')
+env.assert_string('(line (integer 4) (integer 5) )')
+env.assert_string('(line (integer 5) (integer 5) )')
+env.assert_string('(line (integer 6) (integer 5) )')
 
 
 
 
 # Run
-env.run(100)
+env.run(200)
 
 
 # Check facts
