6fe94b0d10cd2cad6f6380c55ace1851d2bc854f
I Putu Gede Wirasuta <wirasutat@gmail.com>
2019-12-10 21:00:45 +0700

    fix rule and naming

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ from pprint import pprint
 from transform import euclidDistance
 import numpy as np
 
+
 def infer(facts):
     m, lines, adjacent_lines, points = facts
     # Initialize
@@ -30,17 +31,19 @@ def infer(facts):
         gradient = m[id_line] if m[id_line] != np.inf else 10 ** 5
 
         id_lines[tuple(line.flat)] = id_line
-        env.assert_string(f'(line (integer {id_line}) (integer {length}) (integer {gradient}))')
-    
+        env.assert_string(
+            f'(line (integer {id_line}) (integer {length}) (integer {gradient}))')
+
     for adj in adjacent_lines:
         tupled_adj_line = tuple(adj)
         meet_string = f'(meet (integer {id_lines[tupled_adj_line[0]]}) (integer {id_lines[tupled_adj_line[1]]}))'
+        env.assert_string(meet_string)
+        meet_string = f'(meet (integer {id_lines[tupled_adj_line[1]]}) (integer {id_lines[tupled_adj_line[0]]}))'
         env.assert_string(meet_string)
 
     # Assert points
     for point in points:
         env.assert_string(f'(point (integer {point[0]}) (integer {point[1]}))')
-
 
     # Run
     env.run(200)
@@ -51,8 +54,9 @@ def infer(facts):
         # <class 'clips.facts.ImpliedFact'>
         # Convert to string
         fact_string += str(fact) + '\n'
-    
+
     return fact_string
+
 
 def rules():
     # Initialize
@@ -64,12 +68,12 @@ def rules():
     with open('shapes.clp', 'r') as file:
         string = file.read().replace('\n', '')
         rules = string.split(';')
-    
+
     for rule in rules:
         env.build(rule)
 
     str_rules = '----RULES----\n'
     for rule in env.rules():
         str_rules += str(rule)
-    
+
     return str_rules
